30675d7280e790e1c0adfa0ad1fe894a50d6be84
Scott Blue <scotch16@gmail.com>
2023-09-25 02:01:05 -0500

    Adding initial components of the Windows ISO build.

---
+++ files/InstallUpdates.ps1
@@ -0,0 +1,3 @@
+Get-PackageProvider -name nuget -force
+Install-Module PSWindowsUpdate -confirm:$false -force
+Get-WindowsUpdate -MicrosoftUpdate -install -IgnoreUserInput -acceptall -AutoReboot | Out-File -filepath 'c:\windowsupdate.log' -append

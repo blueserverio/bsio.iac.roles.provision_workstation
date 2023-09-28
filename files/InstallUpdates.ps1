406bdd2571327105727ed1b3e4598f83a605f02c
Scott Blue <scotch16@gmail.com>
2023-09-27 23:01:04 -0500

    Removed the patching of the OS as part of the OS Install.

--- files/InstallUpdates.ps1
+++
@@ -1,3 +0,0 @@
-Get-PackageProvider -name nuget -force
-Install-Module PSWindowsUpdate -confirm:$false -force
-Get-WindowsUpdate -MicrosoftUpdate -install -IgnoreUserInput -acceptall -AutoReboot | Out-File -filepath 'c:\windowsupdate.log' -append
